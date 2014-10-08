3e016739c8d289e00d39e6c107270de79f6e4b61
deniswhite8 <deniswhite8@gmail.com>
2014-10-08 12:07:13 +0400

    Add try/catch to install

--- app/code/local/Oggetto/FastViewGallery/sql/oggetto_fastViewGallery_setup/mysql4-install-0.1.0.php
+++ app/code/local/Oggetto/FastViewGallery/sql/oggetto_fastViewGallery_setup/mysql4-install-0.1.0.php
@@ -2,10 +2,14 @@
 /** @var $installer Mage_Eav_Model_Entity_Setup */
 $installer = $this;
 
-$entityTypeId = $installer->getEntityTypeId('catalog_product');
+try {
+    $entityTypeId = $installer->getEntityTypeId('catalog_product');
 
-$attributeId = $installer->getAttribute($entityTypeId, 'media_gallery', 'attribute_id');
-$installer->updateAttribute($entityTypeId, $attributeId, array(
-    'backend_type' => 'text',
-    'used_in_product_listing' => 1
-));
+    $attributeId = $installer->getAttribute($entityTypeId, 'media_gallery', 'attribute_id');
+    $installer->updateAttribute($entityTypeId, $attributeId, array(
+        'backend_type' => 'text',
+        'used_in_product_listing' => 1
+    ));
+} catch (Exception $e) {
+    Mage::logException($e);
+}
